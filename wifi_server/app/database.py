9b2f8f96daad090e6c5eb53b756c98f3224d6a3d
austin <galaky2005@gmail.com>
2021-08-13 19:47:37 -0400

    devices can announce themselves; clients are auto-inserted; devices can send data to be transmitted to a specific other device; transmissions can be dequeued; need to allow dequeuer to reply back that transmission was successful so that next message or same device can be dequeued; need to block transmission to device from being dequeued if previous transmission not yet completed;

--- wifi_server/app/database.py
+++ wifi_server/app/database.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from enum import IntEnum
 import sqlite3
 import uuid
+from datetime import datetime
 
 
 class Purpose(IntEnum):
@@ -26,26 +27,66 @@ class Client():
 
 class Device():
 
-	def __init__(self, *, device_id: int, unique_id: str, purpose_id: int):
+	def __init__(self, *, device_guid: str, purpose_id: int):
 
-		self.__device_id = device_id
-		self.__unique_id = unique_id
+		self.__device_guid = device_guid
 		self.__purpose_id = purpose_id
 
-	def get_device_id(self) -> int:
-		return self.__device_id
-
-	def get_unique_id(self) -> str:
-		return self.__unique_id
+	def get_device_guid(self) -> str:
+		return self.__device_guid
 
 	def get_purpose_id(self) -> int:
 		return self.__purpose_id
 
 	def to_json(self) -> object:
 		return {
-			"database_id": self.__device_id,
-			"unique_id": self.__unique_id,
+			"device_guid": self.__device_guid,
 			"purpose_id": self.__purpose_id
+		}
+
+
+class Transmission():
+
+	def __init__(self, *, transmission_id: int, source_device_guid: str, source_client_guid: str, transmission_json_string: str, destination_device_guid: str, dequeue_client_guid: str, sent_guid: str):
+
+		self.__transmission_id = transmission_id
+		self.__source_device_guid = source_device_guid
+		self.__source_client_guid = source_client_guid
+		self.__transmission_json_string = transmission_json_string
+		self.__destination_device_guid = destination_device_guid
+		self.__dequeue_client_guid = dequeue_client_guid
+		self.__sent_guid = sent_guid
+
+	def get_transmission_id(self) -> int:
+		return self.__transmission_id
+
+	def get_source_device_guid(self) -> str:
+		return self.__source_device_guid
+
+	def get_source_client_guid(self) -> str:
+		return self.__source_client_guid
+
+	def get_transmission_json_string(self) -> str:
+		return self.__transmission_json_string
+
+	def get_destination_device_guid(self) -> str:
+		return self.__destination_device_guid
+
+	def get_dequeue_client_guid(self) -> str:
+		return self.__dequeue_client_guid
+
+	def get_sent_guid(self) -> str:
+		return self.__sent_guid
+
+	def to_json(self) -> object:
+		return {
+			"transmission_id": self.__transmission_id,
+			"source_device_guid": self.__source_device_guid,
+			"source_client_guid": self.__source_client_guid,
+			"transmission_json_string": self.__transmission_json_string,
+			"destination_device_guid": self.__destination_device_guid,
+			"dequeue_client_guid": self.__dequeue_client_guid,
+			"sent_guid": self.__sent_guid
 		}
 
 
@@ -60,31 +101,82 @@ class Database():
 	def __initialize(self):
 
 		_cursor = self.__connection.cursor()
-		_cursor.execute("CREATE TABLE device (device_id INTEGER PRIMARY KEY AUTOINCREMENT, unique_id TEXT, purpose_id INTEGER)")
-		_cursor.execute("CREATE TABLE client (client_guid GUID PRIMARY KEY, ip_address TEXT)")
+		_cursor.execute("CREATE TABLE device (device_guid GUID PRIMARY KEY, purpose_id INTEGER, last_known_datetime REAL, is_active INTEGER)")
+		_cursor.execute("CREATE TABLE client (client_guid GUID PRIMARY KEY, ip_address TEXT, UNIQUE(ip_address))")
+		_cursor.execute("CREATE TABLE transmission (transmission_id INTEGER PRIMARY KEY AUTOINCREMENT, source_device_guid GUID, source_client_guid GUID, transmission_json_string TEXT, destination_device_guid GUID, dequeue_client_guid GUID, sent_guid GUID)")
 
 	def insert_client(self, *, ip_address: str) -> Client:
 
 		_client_guid = str(uuid.uuid4())
 
-		_cursor = self.__connection.cursor()
-		_cursor.execute("INSERT INTO client VALUES (?, ?)", (_client_guid, ip_address))
+		_insert_cursor = self.__connection.cursor()
+		_insert_cursor.execute("INSERT OR IGNORE INTO client (client_guid, ip_address) VALUES (?, ?)", (_client_guid, ip_address))
+
+		_get_guid_cursor = self.__connection.cursor()
+		_get_guid_result = _get_guid_cursor.execute("SELECT client_guid FROM client WHERE ip_address = ?", (ip_address, ))
+
+		_saved_guid = _get_guid_result.fetchall()[0][0]
 
 		_client = Client(
-			client_guid=_client_guid,
+			client_guid=_saved_guid,
 			ip_address=ip_address
 		)
 		return _client
 
-	def insert_device(self, *, unique_id: str, purpose_id: int) -> Device:
+	def insert_device(self, *, device_guid: str, purpose_id: int) -> Device:
 
-		_cursor = self.__connection.cursor()
-		_cursor.execute("INSERT INTO device VALUES (?, ?, ?)", (None, unique_id, purpose_id))
-		_device_id = _cursor.lastrowid
+		_insert_cursor = self.__connection.cursor()
+		_insert_cursor.execute("INSERT OR IGNORE INTO device (device_guid, purpose_id, last_known_datetime, is_active) VALUES (?, ?, ?, ?)", (device_guid, purpose_id, datetime.utcnow(), 1))
+
+		_update_known_datetime_cursor = self.__connection.cursor()
+		_update_known_datetime_cursor.execute("UPDATE device SET last_known_datetime = ? WHERE device_guid = ?", (datetime.utcnow(), device_guid))
 
 		_device = Device(
-			device_id=_device_id,
-			unique_id=unique_id,
+			device_guid=device_guid,
 			purpose_id=purpose_id
 		)
 		return _device
+
+	def insert_transmission(self, *, source_device_guid: str, client_guid: str, transmission_json_string: str, destination_device_guid: str) -> Transmission:
+
+		_insert_cursor = self.__connection.cursor()
+		_insert_cursor.execute("INSERT INTO transmission (transmission_id, source_device_guid, source_client_guid, transmission_json_string, destination_device_guid, dequeue_client_guid, sent_guid) VALUES (?, ?, ?, ?, ?, ?, ?)", (None, source_device_guid, client_guid, transmission_json_string, destination_device_guid, None, None))
+		_transmission_id = _insert_cursor.lastrowid
+
+		_transmission = Transmission(
+			transmission_id=_transmission_id,
+			source_device_guid=source_device_guid,
+			source_client_guid=client_guid,
+			transmission_json_string=transmission_json_string,
+			destination_device_guid=destination_device_guid,
+			dequeue_client_guid=None,
+			sent_guid=None
+		)
+		return _transmission
+
+	def get_next_transmission(self, *, client_guid: str) -> Transmission:
+
+		_sent_guid = str(uuid.uuid4())
+
+		_update_cursor = self.__connection.cursor()
+		_update_cursor.execute("UPDATE transmission SET dequeue_client_guid = ?, sent_guid = ? WHERE transmission_id = (SELECT t_inner.transmission_id FROM transmission AS t_inner WHERE t_inner.sent_guid IS NULL ORDER BY t_inner.transmission_id LIMIT 1)", (client_guid, _sent_guid))
+
+		_get_cursor = self.__connection.cursor()
+		_get_result = _get_cursor.execute("SELECT transmission_id, source_device_guid, source_client_guid, transmission_json_string, destination_device_guid FROM transmission WHERE sent_guid = ?", (_sent_guid, ))
+
+		_rows = _get_result.fetchall()
+		if len(_rows) == 0:
+			_transmission = None
+		else:
+			_row = _rows[0]
+
+			_transmission = Transmission(
+				transmission_id=_row[0],
+				source_device_guid=_row[1],
+				source_client_guid=_row[2],
+				transmission_json_string=_row[3],
+				destination_device_guid=_row[4],
+				dequeue_client_guid=client_guid,
+				sent_guid=_sent_guid
+			)
+		return _transmission
--- wifi_server/app/main.py
+++ wifi_server/app/main.py
@@ -20,23 +20,99 @@ def test_root():
 	return {"is_successful": True}
 
 
-@app.get("/announce")
-def receive_device_announcement(unique_id: str, purpose_id: int, request: Request):
+@app.post("/announce")
+def receive_device_announcement(device_guid: str, purpose_id: int, request: Request):
+
 	_is_successful = False
-	_device_json = None
+	_response_json = None
+	_error_message = None
+
 	try:
 		_database = get_database()
 		_client = _database.insert_client(
 			ip_address=request.client.host
 		)
 		_device = _database.insert_device(
-			unique_id=unique_id,
+			device_guid=device_guid,
 			purpose_id=purpose_id
 		)
-		_device_json = _device.to_json()
-	except:
+		_response_json = {
+			"device": _device.to_json()
+		}
+		_is_successful = True
+	except Exception as ex:
+		_error_message = str(ex)
 		traceback.print_exc()
+
 	return {
 		"is_successful": _is_successful,
-		"device": _device_json
+		"response": _response_json,
+		"error": _error_message
+	}
+
+
+@app.post("/transmit")
+def receive_device_transmission(source_device_guid: str, transmission_json_string: str, destination_device_guid: str, request: Request):
+
+	_is_successful = False
+	_response_json = None
+	_error_message = None
+
+	try:
+		_database = get_database()
+		_client = _database.insert_client(
+			ip_address=request.client.host
+		)
+		_transmission = _database.insert_transmission(
+			source_device_guid=source_device_guid,
+			client_guid=_client.get_client_guid(),
+			transmission_json_string=transmission_json_string,
+			destination_device_guid=destination_device_guid
+		)
+		_response_json = {
+			"transmission": _transmission.to_json()
+		}
+		_is_successful = True
+	except Exception as ex:
+		_error_message = str(ex)
+		traceback.print_exc()
+
+	return {
+		"is_successful": _is_successful,
+		"response": _response_json,
+		"error": _error_message
+	}
+
+
+@app.post("/dequeue")
+def dequeue_next_transmission(request: Request):
+
+	_is_successful = False
+	_response_json = None
+	_error_message = None
+
+	try:
+		_database = get_database()
+		_client = _database.insert_client(
+			ip_address=request.client.host
+		)
+		_transmission = _database.get_next_transmission(
+			client_guid=_client.get_client_guid()
+		)
+		if _transmission is None:
+			_transmission_json = None
+		else:
+			_transmission_json = _transmission.to_json()
+		_response_json = {
+			"transmission": _transmission_json
+		}
+		_is_successful = True
+	except Exception as ex:
+		_error_message = str(ex)
+		traceback.print_exc()
+
+	return {
+		"is_successful": _is_successful,
+		"response": _response_json,
+		"error": _error_message
 	}
